2c0fb1c407779a522d78b5eaa267ea84fe7dd7ed
Jair de Jesus Garcia Hernandez <jairhdz.6838@gmail.com>
2022-03-30 22:15:50 -0600

    Primer Departamental

--- JS/Bucles/Bucle_Do-While.js
+++ JS/Bucles/Bucle_Do-While.js
@@ -2,7 +2,7 @@
 var i=0;  
 do  
 {  
-    document.write("Repetición en estrada web group número: " +i +"<br/>");  
+    document.write("Repeticion de un numero: " +i +"<br/>");  
     i++;  
 }  
-while(i<=10) 
+while(i<=40) 
